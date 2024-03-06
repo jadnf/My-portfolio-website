1c125a5dfc18017af547b519c1cac2f4f037e2fe
J. Jadan fox <160644667+jadnf@users.noreply.github.com>
2024-03-06 16:48:37 -0700

    ough

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-var inspiroBotQuote = document.querySelector("#inspiro-bot-quote")
+var quote = document.querySelector("#quote");
+var slideshow = document.querySelector("#slideshow");
 
 fetch('https://api.kanye.rest').then(response => {
     response.json().then(data => {
-        console.log(data)
-        inspiroBotQuote.textContent = data.quote
+        quote.textContent = data.quote
     })
 })
-console.log(output)
+var i = 0;
+var images = ["images/roomnoises.jpg", "images/Him.jpg", "images/ output-onlinegiftools.gif"]
+setInterval(() => {
+    
+    i++
+    slideshow.src = images[i]
+    if(i == images.length){
+        i =0
+    }
+}, 1000);
