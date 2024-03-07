ab06b87b8bf27030814465003454d155e4eadaff
J. Jadan fox <160644667+jadnf@users.noreply.github.com>
2024-03-06 18:21:38 -0700

    nbgfxhdfghsdfgjjdghf

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 var quote = document.querySelector("#quote");
 var slideshow = document.querySelector("#slideshow");
+var author = document.querySelector("#author");
 
-fetch('https://api.kanye.rest').then(response => {
-    response.json().then(data => {
-        quote.textContent = data.quote
+fetch('https://api.breakingbadquotes.xyz/v1/quotes')
+    .then(response => response.json())
+    .then(data => {
+        quote.textContent = data[0].quote
+        author.textContent = data[0].author
     })
-})
 var i = 0;
-var images = ["images/roomnoises.jpg", "images/Him.jpg", "images/ output-onlinegiftools.gif"]
+var images = ["images/roomnoises.jpg", "images/Him.jpg", "images/output-onlinegiftools.gif", "images/funny.gif"]
 setInterval(() => {
-    
-    i++
-    slideshow.src = images[i]
-    if(i == images.length){
-        i =0
+    if(i > images.length-1){
+        i=0;
     }
-}, 1000);
+    slideshow.src = images[i]
+    i += 1;
+}, 2000);
